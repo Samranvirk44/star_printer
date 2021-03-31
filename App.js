9498a051e66be758f1117751f8910cb593523172
Kashmir <samranvirk44@gmail.com>
2021-03-31 10:34:57 +0500

    changeing

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 //import liraries
 import React, { Component, useState } from 'react';
-import { View, Text, Image, ScrollView, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet, TextInput, TouchableOpacity, Platform, NativeAppEventEmitter } from 'react-native';
 import { StarPRNT } from 'react-native-star-prnt';
-
+import BleManager from 'react-native-ble-manager';
+import img from './assents/img.jpg'
 // create a component
 const MyComponent = () => {
-  const [printerss, setprinters] = useState([{modelName:""}]);
+  const [printerss, setprinters] = useState([{ modelName: "" }]);
   const [portt, setportt] = useState("");
   const [connectt, setconn] = useState("");
   const [printt, setprintt] = useState("");
 
-  async function checkstatus(){
-    try{
-      let status=await StarPRNT.checkStatus("TCP:192.168.1.1", "EscPosMobile")
-alert(status)
-console.log(status)
+  async function checkstatus() {
+    try {
+      let status = await StarPRNT.checkStatus("TCP:00.11.62.18:30:7C", "EscPosMobile")
+      alert(status)
+      console.log(status)
     }
-    catch(err){
+    catch (err) {
       alert(err)
       console.log(err)
     }
+  }
+  async function checkbluetooth() {
+    console.log('-------------->')
+    NativeAppEventEmitter.addListener('BleManagerDiscoverPeripheral', (data) => {
+      console.log(data) // Name of peripheral device
+    });
+
+    let data = BleManager.start({ showAlert: false });
+    console.log(data)
+
+    // BleManager.scan([], 30)
   }
   async function portDiscovery() {
     console.log('port finding')
@@ -28,6 +40,10 @@ console.log(status)
     try {
       let printers = await StarPRNT.portDiscovery('All');  //used for discover printers
       console.log('printers--->', printers);
+      console.log(printers[0].macAddress)
+      console.log(printers[0].modelName)
+      console.log(printers[0].portName)
+
       setprinters(printers);
       //let prineter = [{ modelName: "TSP700II", macAddress: "00:00:00:00", portName: "TCP:192.168.1.1" }]
       // console.log(prineter[0].modelName)
@@ -40,16 +56,33 @@ console.log(status)
       //   openCashDrawer: "true"
       // }
       if (printers.length != 0) {
+
         StarPRNT.connect(printers[0].portName, "StarPRNT", false)
           .then(res => {
-            console.log("response=>", res);
-            setconn(res);
-            StarPRNT.print("StarPRNT", [{ append: "text" }, { "openCashDrawer": 1 }], printers[0].portName)
-              .then(print => { console.log("print=>", print)
-            setprintt(print) })
-              .catch(err => console.log("print err=>", err))
+            console.log("connected ---=>", res);
+
+            let commands = [];
+            commands.push({
+              appendBitmap:img});
+            commands.push({ appendCutPaper: StarPRNT.CutPaperAction.PartialCutWithFeed });
+          console.log(commands)
+            StarPRNT.print("StarPRNT", commands,printers[0].portName)
+              .then(result => {
+                console.log(result)
+              })
+              .catch(err => console.log("err=>", err))
+
+
+            // setconn(res);
+            // StarPRNT.print("StarPRNT", [{ append: "text" }, { "openCashDrawer": 1 }], printers[0].portName)
+            //   .then(print => { console.log("print=>", print)
+            // setprintt(print) })
+            //   .catch(err => console.log("print err=>", err))
           })
           .catch(err => console.log("err=>", err))
+
+
+
         //     StarPRNT.printRawText(printers[0].portName,printers[0].modelName, printObj, function(result){
 
         // StarPRNT.printRawText("BT:00:11:62:18:30:7C", "BT:TSP100-H1140", printObj, function (result) {
@@ -60,6 +93,8 @@ console.log(status)
         //     console.log("error=>", error);
         //     setoutput(error)
         //   });
+        StarPRNT.disconnect(printers[0].portName, "StarPRNT", false)
+
       }
       else {
         alert('Printer is not connected')
@@ -76,11 +111,14 @@ console.log(status)
       <TouchableOpacity onPress={() => portDiscovery()} style={{ height: 50, width: '70%', borderRadius: 10, marginTop: '20%', alignSelf: 'center', backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
         <Text>Clinck on this for print</Text>
       </TouchableOpacity>
-      <Text>Modal Name   {printerss[0].modelName}</Text>
+      {/* <Text>Modal Name   {printerss[0].modelName}</Text>
       <Text>Connected    {connectt}</Text>
-      <Text>Print        {printt}</Text>
+      <Text>Print        {printt}</Text> */}
       <TouchableOpacity onPress={() => checkstatus()} style={{ height: 50, width: '70%', borderRadius: 10, marginTop: '20%', alignSelf: 'center', backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
         <Text>check status of printer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => checkbluetooth()} style={{ height: 50, width: '70%', borderRadius: 10, marginTop: '20%', alignSelf: 'center', backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
+        <Text>check bluetooth</Text>
       </TouchableOpacity>
     </View>
 
